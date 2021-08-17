16824f7fe8f529f4efb37a93b4a2d7a3e16e3947
Mederic <46417515+medericgb@users.noreply.github.com>
2021-08-17 09:57:30 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
-def parse_csv(file_path)
+def parse_csv(file_path, sep=",")
   content = File.read(file_path)
   lines = content.split("\n")
-  header = lines[0].split(',')
+  header = lines[0].split(sep)
   body = lines[1..-1]
 
   data = body.map do |line|
     hash = Hash.new
-    vals = line.split(",")
+    vals = line.split(sep)
     vals.each_with_index do |val, i|
       hash[header[i]] = val
     end 
